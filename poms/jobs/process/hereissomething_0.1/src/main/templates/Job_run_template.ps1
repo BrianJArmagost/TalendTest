60e1fe966efc1d4808ac13c504c399621180f62d
Jeff Kelly <jekelly@datarecognitioncorp.com>
2018-12-19 15:34:22 -0600

    test

--- poms/jobs/process/hereissomething_0.1/src/main/templates/Job_run_template.ps1
+++ poms/jobs/process/hereissomething_0.1/src/main/templates/Job_run_template.ps1
@@ -1,3 +1,3 @@
 $fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/talend_file_enhanced_20070724.jar;hereissomething_0_1.jar;' new_test_project.hereissomething_0_1.HereIsSomething  %*
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/postgresql-42.2.5.jar;../lib/talend_file_enhanced_20070724.jar;hereissomething_0_1.jar;' testgit.hereissomething_0_1.HereIsSomething  %*
